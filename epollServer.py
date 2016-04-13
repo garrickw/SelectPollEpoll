98d1d4e5153c652e6860202b5a8fff13db526311
garrick <wzsgarrick@gmail.com>
2016-04-13 23:50:58 +0800

    fix the endless-loop problem

--- epollServer.py
+++ epollServer.py
@@ -1,5 +1,4 @@
 """ An example about how to use linux epoll in python """
-
 import socket
 import select
 from wsgiref.handlers import format_date_time
@@ -18,17 +17,19 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind(server_addr)
 sock.setblocking(0)
-sock.listen(5)
+sock.listen(1024)
 
 # create an epoll object
 epoll = select.epoll()
 # set edge_trigger
 epoll.register(sock.fileno(), select.EPOLLIN | select.EPOLLET)
 
+
 try:
     connections = {}
     requests = {}
     responses = {}
+    addr = {}
 
     while True:
         events = epoll.poll(1)  # 1s timeout
@@ -41,12 +42,14 @@ try:
                         print "A new connection is coming...", client_addr
                         conn.setblocking(0)
                         epoll.register(conn.fileno(), select.EPOLLIN)
+                        addr[conn.fileno()] = client_addr
                         connections[conn.fileno()] = conn
                         requests[conn.fileno()] = b''
                         responses[conn.fileno()] = response
                 except socket.error, e:
-                    # because of non-blocking, accept will return error
+                    # because of non-blocking, accept will return error immediately
                     # when no connection comes
+                    print "accept fail"
                     pass
 
             elif event & select.EPOLLIN:
@@ -55,12 +58,17 @@ try:
                         data = connections[fileno].recv(1024)
                         if data:
                             requests[fileno] += data
+                        elif not requests[fileno]:
+                            epoll.modify(fileno, 0)
+                            connections[fileno].shutdown(socket.SHUT_RDWR)
+                            break
 
                         # check wether all datas were received or not
                         if any([eol in data for eol in EOLS]):
                             epoll.modify(fileno, select.EPOLLOUT)
                             print('-'*79 + '\n' + requests[fileno].decode()[:-2])
                 except socket.error:
+                    print "error in"
                     pass
 
             elif event & select.EPOLLOUT:
@@ -72,15 +80,16 @@ try:
                             epoll.modify(fileno, 0)
                             connections[fileno].shutdown(socket.SHUT_RDWR)
                 except socket.error:
-                    print "error"
+                    print "error out"
                     pass
 
             elif event & select.EPOLLHUP:
+                print fileno
                 epoll.unregister(fileno)
+                o += 1
                 connections[fileno].close()
                 del connections[fileno]
                 del responses[fileno]
-                del requests[fileno]
 finally:
     epoll.unregister(sock.fileno())
     epoll.close()
